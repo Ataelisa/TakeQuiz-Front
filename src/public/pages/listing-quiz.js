09541ed550d223d91b6afd95e852967502d558f4
Nouha-06 <nouhaila.mellouki@gmail.com>
2021-12-13 15:18:56 +0100

    :sparkles: commit front Card

--- src/public/pages/listing-quiz.js
+++ src/public/pages/listing-quiz.js
@@ -37,7 +37,7 @@ const ListingQuiz = ({ open, setOpen }) => {
           columns={{ xs: 4, sm: 8, md: 12 }}
         >
           {Array.from(Array(9)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
+            <Grid item xs={2} sm={3} md={3} key={index}>
               <QuizCard title={`card ${index}`}></QuizCard>
             </Grid>
           ))}
