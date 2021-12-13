00d8f2c006fd763297481663324ae5d6d514ced9
Nouha-06 <nouhaila.mellouki@gmail.com>
2021-12-13 15:17:02 +0100

    :sparkles: Commit front Card

--- src/public/components/quiz-card.js
+++ src/public/components/quiz-card.js
@@ -3,8 +3,12 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import  IconButton  from '@mui/material/IconButton';
+import  CardMedia  from '@mui/material/CardMedia';
+import Avatar  from '@mui/material/Avatar';
+import { red } from '@mui/material/colors';
+import  CardHeader  from '@mui/material/CardHeader';
 
 const bull = (
   <Box
@@ -17,25 +21,32 @@ const bull = (
 
 export default function QuizCard() {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ maxWidth: 345 }}
+        
+      >
+      <CardHeader
+        action={
+          <IconButton aria-label="settings">
+          </IconButton>
+        }
+        title="Name"
+        subheader="Thème"
+      />
+     
+      <CardMedia
+        component="img"
+        height="194"
+        image="https://th.bing.com/th/id/R.33d02c67b4a6e90abe2d7a58f764edd8?rik=gA%2fesQP2%2f0%2b5uw&riu=http%3a%2f%2fwww.snut.fr%2fwp-content%2fuploads%2f2015%2f12%2fimage-de-nature-9.jpg&ehk=4oiNLekZZh50XowVszovQmq8w%2fH0S6GIwQYqeKknWaM%3d&risl=&pid=ImgRaw&r=0"
+        alt="Paella dish"
+      />
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
+      <CardActions disableSpacing>
       </CardActions>
     </Card>
   );
