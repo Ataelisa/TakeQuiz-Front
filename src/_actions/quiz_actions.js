1b1b3f143d658b95022e1f54c7066a1f459d8af3
abdellah098 <saley.abdellah@gmail.com>
2021-12-29 10:56:38 +0100

    [feat] ajout de la dépendance json-server pour simuler les api

--- src/_actions/quiz_actions.js
+++ src/_actions/quiz_actions.js
@@ -1,15 +1,10 @@
 import axios from "axios";
-const baseUrl = 'https://localhost:44315/Quiz'
+const baseUrl = " http://localhost:3000/quiz"; //'https://localhost:44315/Quiz'
 
 export function getListQuiz() {
-  return axios
-    .get(baseUrl)
-    .then((response) => response.data)
+  return axios.get(baseUrl).then((response) => response.data);
 }
 
 export function postQuiz(quiz) {
-  return axios
-    .post(baseUrl, quiz)
-    .then((response) => response.data)
+  return axios.post(baseUrl, quiz).then((response) => response.data);
 }
-
